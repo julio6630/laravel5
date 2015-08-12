1adac7da78ad8b61765aa13c03c257adfaf210d3
Julio Enrique Carbajal <jekar.6630@gmail.com>
2015-08-12 13:06:45 -0500

    Ticket and TicketVote new models and migrations

--- database/migrations/2015_08_12_175148_create_tickets_table.php
+++ database/migrations/2015_08_12_175148_create_tickets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateTicketsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,13 +12,13 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('usersl', function(Blueprint $table)
+		Schema::create('tickets', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('username');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
+			$table->string('title',200);
+			$table->enum('status', ['open','closed']);
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('usersl');
 			$table->timestamps();
 		});
 	}
@@ -30,7 +30,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('tickets');
 	}
 
 }
